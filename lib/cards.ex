6b4ea68d865a04bca74dc9f412f159762b4e4b98
Christopher Miller <camiller.yr@gmail.com>
2018-05-20 17:32:29 -0500

    basic deck functionality added

--- lib/cards.ex
+++ lib/cards.ex
@@ -12,7 +12,49 @@ defmodule Cards do
       :world
 
   """
-  def hello do
-    :world
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spade", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits,
+        value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, file_name) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(file_name, binary)
+  end
+
+  def load(file_name) do
+    File.read(file_name)
+    |> parse_load
+  end
+
+  def parse_load({:ok, binary}) do
+    :erlang.binary_to_term(binary)
+  end
+
+  def parse_load({:error, _reason}) do
+    "That file does not exist"
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
   end
 end
