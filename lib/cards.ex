3df247fcfe644e3d71a0b3aa7188b17978430877
Christopher Miller <camiller.yr@gmail.com>
2018-05-24 15:49:34 -0500

    Added dock and case tests

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,20 +1,14 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for Cards.
+  Provides methods for creating and handling a deck of cards
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello
-      :world
-
+  Retruns a collection of strings representing a deck of cards
   """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
-    suits = ["Spade", "Clubs", "Hearts", "Diamonds"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
     for suit <- suits,
         value <- values do
@@ -22,36 +16,78 @@ defmodule Cards do
     end
   end
 
+
+  @doc """
+  Takes in a deck of cards and returns a new deck in a random order
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+  Takes a collection of cards and a single card and checks the collection for the exsistence of the single
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Five of Hearts")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  
+  @doc """
+  Returns a list of cards as dictated by the `hand_size` and a collection of the remaining cards in the deck
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, _deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
-
+  
+  @doc """
+  Takes in a collection of cards and a file name and saves the collection to the file sysytem
+  """
   def save(deck, file_name) do
     binary = :erlang.term_to_binary(deck)
     File.write(file_name, binary)
   end
 
+  @doc """
+  Takes in a file name and loads the collection of cards from the file, or returns an error
+  """
   def load(file_name) do
     File.read(file_name)
     |> parse_load
   end
 
-  def parse_load({:ok, binary}) do
+  @doc """
+  Private function for reading file from the file system if File.read returns :ok
+  """
+  defp parse_load({:ok, binary}) do
     :erlang.binary_to_term(binary)
   end
 
-  def parse_load({:error, _reason}) do
+  @doc """
+  Private function for reading file from the file system if File.read returns :error
+  """
+  defp parse_load({:error, _reason}) do
     "That file does not exist"
   end
 
+  @doc """
+  Takes an integer value that dictates the numbe of cards you wish to receive - creates a new deck, shuffles the deck into a random order,
+  and returns a tuple {hand, remaining_deck}
+  """
   def create_hand(hand_size) do
     Cards.create_deck()
     |> Cards.shuffle()
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,20 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create_deck returns 20 cards" do
+    deck_length = length(Cards.create_deck())
+    assert deck_length == 20
   end
+
+  test "create_hand returns the correct number of cards in the hand" do
+    {hand, deck} = Cards.create_hand(5)
+    assert length(hand) == 5
+    assert length(deck) == 15
+  end
+
+  test "shuffle randomizes deck" do
+    deck = Cards.create_deck
+    assert deck != Cards.shuffle(deck)
+  end
+
 end
